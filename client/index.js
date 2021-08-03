7885dfc95456041c4b06d9022b123a624cc73e96
Sophia Cheong <sophiaacheong@gmail.com>
2021-08-02 19:46:01 -0700

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,34 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongCreate from './components/SongCreate';
+import SongList from './components/SongList';
+import SongDetail from './components/SongDetail';
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
