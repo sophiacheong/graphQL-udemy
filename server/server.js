8a7fdcce6791cfff16a588a2517f7346941b55eb
sophiacheong <sophiaacheong@gmail.com>
2021-08-02 20:03:18 -0700

    added server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 
 const app = express();
-// Replace with your mongoLab URI
+
 const MONGO_URI = `mongodb+srv://p00gz:${token}@cluster0.1wjg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
